46fb9269e3a87709f7e64125405a36aa8fafb1ee
Willie Suggs <klbooker09@gmail.com>
2017-05-10 23:09:00 -0500

    Add files via upload

---
+++ ForLoopChallenge.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forloopchallenge;
+
+/**
+ *
+ * @author kendrabooker
+ */
+public class ForLoopChallenge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        for(int j=15; j <= 30; j += 3){
+        
+            System.out.println(j);
+        }
+    }
+    
+}
